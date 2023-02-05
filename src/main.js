b2c3913843612bf71d481cc1b83aed5642c40656
zakary dujardin <zakoduj@gmail.com>
2023-02-04 23:28:22 -0500

    initialisation index

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
 
-createApp(App).mount('#app')
+new Vue({
+  data: {
+    message: 'Bonjour, monde !'
+  },
+  template: `
+    <div>
+      {{ message }}
+    </div>
+  `
+}).$mount('#app')
